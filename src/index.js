c25ae8cadfea8f9f1192de3827070e75dc58dfcc
mrpatblack7 <45779399+mrpatblack7@users.noreply.github.com>
2021-12-25 00:19:02 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import "./styles.css";
+
+/* document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`; 
+
+link for project tutorial - https://www.youtube.com/watch?v=3PHXvlpOkf4&t=1825s
+*/
+// set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "black";
+    }
+    value.textContent = count;
+  });
+});
